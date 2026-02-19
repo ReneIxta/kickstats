e06f405c00d75d121e7a3a9e8363d6562c9c13a2
RIxta <rene.ixta@roomnet.com>
2026-02-18 23:47:38 -0700

    Updated App.tsx to include database.ts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { initDatabase } from "./src/storage/database";
 
 export default function App() {
+  useEffect(() => {
+    initDatabase();
+  }, []);
+
   return <AppNavigator />;
 }
